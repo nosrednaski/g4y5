617e2e917d7ccbfd046ffff69042b715aaa0cd55
Jeff <nosrednaski@gmail.com>
2020-12-02 10:55:32 -0500

    init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <input type="search" v-model='query'>
+    <button type="submit" @click='submit'>Submit</button>
+  </div>
+
+  <section>
+    <template v-for="(toDo, i) in toDos" :key='i'>
+      <button @click='done(toDo)' :class="{ 'crossed-out' : toDo.bool }" >
+        {{ toDo.title }}
+      </button>
+      
+    </template>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      query: null,
+      toDos: [],
+    }
+  },
+  methods: {
+    submit() {
+      this.toDos.push({
+        title: this.query,
+        bool: false
+      })
+    },
+    done(toDo) {
+      toDo.bool = !toDo.bool
+    }
+  },
+  async created() {
+    const res = await fetch("https://todo-cf583.firebaseio.com/")
+    console.log(res)
   }
 }
 </script>
@@ -22,5 +48,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+input {
+  margin-bottom: 4vh;
+  margin-right: 1vw;
+}
+section > button {
+  display: block;
+  margin: .5rem auto;
+}
+.crossed-out {
+  text-decoration: line-through;
 }
 </style>
